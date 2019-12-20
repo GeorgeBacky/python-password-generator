35e588885fc8fb3f30dadc0af466e647be1b2f43
George Bakalis <36627616+georgebak2182@users.noreply.github.com>
2019-12-20 21:49:10 +0200

    Update PythonPassGen.py

--- PythonPassGen.py
+++ PythonPassGen.py
@@ -2,7 +2,7 @@ import os, random, string
  
 def get_random_string(min_chars=10, max_chars=15):
     all_chars = string.ascii_letters + string.digits + string.punctuation
-    random.seed = (os.urandom(1024))  # this line can be removed
+    random.seed = (os.urandom(1024))  # Υou can remove this line if you want!
     random_str = "".join(random.choice(all_chars)
     for _ in range(random.randint(min_chars, max_chars)))
     return random_str
